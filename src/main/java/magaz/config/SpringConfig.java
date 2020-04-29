66dd4f722e6f8052df6436b401bb685570f86ae5
Mihail <mischa.bobylev@gmail.com>
2020-04-29 13:14:23 +0300

    мини-магазин

---
+++ src/main/java/magaz/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package magaz.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("magaz.model")
+public class SpringConfig {
+}
